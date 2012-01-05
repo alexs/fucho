2bc924c5b73c9b9d61f8abec18c056404634ef38
Alex Silva <alexs1010@gmail.com>
2012-01-04 18:11:20 -0600

    Start the project =D

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fucho::Application.config.secret_token = 'd48371db8f30c9a8fa4faeeb7673930ca228d8ca889daaf5fee864c5ff6215d931449a64e624120a025c49820447e43e1134740ba2b2609ee5d222bc33f115fc'
